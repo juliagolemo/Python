913e15fa3ae60e17c9079a3ea8062156c9479e9c
Julia <juliagolemo61@gmail.com>
2024-11-30 18:23:18 +0000

    Variable

---
+++ main.py
@@ -0,0 +1,30 @@
+print('Hallo Welt!')
+print("Ich habe Hallo Welt programmiert")
+
+# Variable 
+x = 5
+# Wyświetla się nam 5
+print(x)
+
+# Wyświetla się nam x 
+print("x")
+
+# Bei Variabeln geben wir ZUERST x, dann print(...)
+
+# Um etwas einzuschalten, geben wir "pyhon main.py" ein
+
+# Uebung 1
+firmenname = "Lufthansa"
+print("Hallo liebes Team von " + firmenname + " ,ich moechte gerne mit euch in der Firma " + firmenname + " arbeiten.")
+
+# Uebung 2 - fuege einen zweiten Aufgabebefehl mit der Variable berufsfeld hinzu, der zu deinen Wunschberuf zuweist.
+firmenname = "Lufthansa"
+berufsfeld = "Junior HR Advisor"
+weiterbildungsdauer = "2"
+print("Ich habe einen Weiterbildung zum " + berufsfeld + "gemacht. ")
+print(weiterbildungsdauer)
+
+# Pisząc weiterbildungsdauer = 2, wyświetli się nam 2
+# Pisząc weiterbildungsdauer = 2 - 1, wyświetli się nam 1
+# Pisząc "2 - 1", wyświetli się nam "2 - 1"
+
