84b6fe5195844d0af0fbaa97bea707605d176fa7
Julia <juliagolemo61@gmail.com>
2024-12-01 16:57:48 +0000

    Add slicing

--- main.py
+++ main.py
@@ -28,3 +28,38 @@ print(weiterbildungsdauer)
 # Pisząc weiterbildungsdauer = 2 - 1, wyświetli się nam 1
 # Pisząc "2 - 1", wyświetli się nam "2 - 1"
 
+# Einfache Datentypen: string, int, float, bool
+
+#string - mając np. napisz Hallo Welt, możemy do każdej litery przypasować liczbę. ZAWSZE zaczynami od 0, a pauzy również są wliczane (litera H będzie miała numer 0)
+
+# Wyświetli się nam napis Hallo Welt
+x = 'Hallo Welt'
+print(x)
+
+# Wyświetli się nam tylko litera W, bo jest 6 w kolejności
+x = 'Hallo Welt'
+print(x[6])
+
+# Wyświetli się nam litera t, bo liczymy od końca (-1)
+x = 'Hallo Welt'
+print(x[-1])
+
+# Wyświetli się nam litera l, bo liczymy od końca (-2)
+x = 'Hallo Welt'
+print(x[-2])
+
+#SLICING - Konzept zum Zerlegen von Sequenzen/Strings anhand ihrer Indizes. Ermoeglich z.B. Isolieren einzelnen Woerter
+
+# Uebung 1 - Chcę "wyciągnąć" słowo Welt z 'Hallo Welt'
+# Piszę 6, bo od 6 zaczyna się to słowo
+# Po : piszemy liczbę, do którego miejsca chcemy wyciągnąć słowo. Jeśli nie damy nic, automatycznie wyciągnie nam wszystko do końca
+x = 'Hallo Welt'
+print(x[6:]) 
+
+
+# Uebung 2 - Chcę "wyciągnąć" słowo Hallo z 'Hallo Welt'
+# Jeśli chcemy zacząc od samego początku, nie musimy podawać przed : liczby. Python automatycznie zacznie od 0.
+# Na 5 chcemy skończyć
+x = 'Hallo Welt'
+print(x[:5]) 
+
