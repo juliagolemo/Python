85b967063a22a97d3541181f70def2117793cfb0
Julia <juliagolemo61@gmail.com>
2024-12-03 09:29:31 +0000

    Add int and float

---
+++ integer_and_float/zahlen.py
@@ -0,0 +1,6 @@
+# INTGER - angegeben als int, Ganzzahlen, positiv oder negativ z.B. 100, 20, 0, -50
+# FLOAT z.B. 3.6, 8.9
+#FUNKTION type - einfache Funktion zur Bestimmung des Datentyps (int, float usw.)
+
+print(10 / 2)
+print(type(10))
