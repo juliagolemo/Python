baf14ae9c6e89cdd45dd32465c8eb4b1f4f6cb02
Szymon Cogiel <81774440+SzymonCogiel@users.noreply.github.com>
2024-12-07 11:10:36 +0000

    Mission 1920

---
+++ schleifen/1920.py
@@ -0,0 +1,8 @@
+# Program losujący liczby w pętli while
+# 1. Wylosuj liczby całkowite z przedzioału 1900-2025 za pomocą funkcji randint() z biblioteki random.
+# 2. Pętla powinna działać dopóki wylosowana liczba nie będzie równa 1920.
+# 3. Każda wylosowana liczba powinna zostać wypisana na ekranie.
+# 4. Gdy wylosowana zostanie liczba 1920, również wypisz "Ino Ruch!!!", a następnie zakończ działanie programu.
+
+# Przykład użycia funkcji randint() znajduje się tutaj:
+# https://www.w3schools.com/python/ref_random_randint.asp
