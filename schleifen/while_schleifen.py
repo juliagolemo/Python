5e460b8733cb65685c260e39ce29c823d29f1ba9
Julia <juliagolemo61@gmail.com>
2024-12-09 17:05:41 +0000

    Add schleifen part 2

--- schleifen/while_schleifen.py
+++ schleifen/while_schleifen.py
@@ -13,6 +13,43 @@ entscheidung = "yes"
 while entscheidung == "yes":
     einkaufsliste.append(input("Was moechtest du deiner Liste hinzufuegen? "))
     entscheidung = input("Moechtest du weitere Artikel hinzufuegen ( yes / no ) ")
+    print(einkaufsliste)
 
-# tbc
-    
+# Projekt
+einkaufsliste = []
+# Artikel hinzufügen
+# Aktion -> hinzufügen/entfernen/anzeigen/beenden
+
+# Endlos-schleife mit BREAK (statt Bedingung am Anfang nur while True, läuft endlos)
+while True:
+    aktion = input("Möchtest du einen Artikel hinzufügen/entfernen oder die Liste anzeigen? (hinzufügen / entfernen / anzeigen / beenden)")
+
+    # hinzufügen
+    if aktion == "hinzufügen":
+        atikel = input("Welchen Artikel möchtest du hinzufügen? ")
+        einkaufsliste.append(artikel)
+        print("Artikel wurde hinzugefügt")
+
+    # löschen
+    elif aktion == "entfernen":
+        artikel = input("Welchen Artikel möchtest du hinzufügen? ")
+        if artikel in einkaufsliste:
+            einkaufsliste.remove(artikel)
+            print("Artikel wurde gelöscht")
+        else:
+            print("Den Artikel gibt es nicht!")
+
+    # anzeigen
+    elif aktion == "anzeigen":
+        print("Deine Einkaufsliste:")
+        print(einkaufsliste)
+    elif aktion == "beenden":
+        print("Einkaufsliste beendet")
+        break
+
+
+2h 30 min
+
+
+
+
