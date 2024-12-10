24b57dce88d5029971fa515dc68c012e3d54f639
Julia <juliagolemo61@gmail.com>
2024-12-10 16:58:40 +0000

    Add Schleifen part 3

--- schleifen/while_schleifen.py
+++ schleifen/while_schleifen.py
@@ -52,3 +52,19 @@ while True:
         print("Du musst das schon richtig eingeben.")
 
 print(einkaufsliste)
+
+# Übung
+from random import randint
+zahl = randint(1,100)
+
+print("Hey ich habe mir eine Zahl zwischen 1 und 100 ausgedacht.")
+while True:
+    versuch = int(input("Rate meine Zahl: "))
+
+    if versuch == zahl:
+        print("Super, du hast die Zahl erraten!")
+        break
+    elif versuch < zahl:
+        print("Die Zahl ist zu klein")
+    else:
+        print("Die Zahl ist zu groß")
