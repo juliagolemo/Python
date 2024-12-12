8f5953f9e3af5d1074804f0e9cbd37cc3bc3c78d
Julia <juliagolemo61@gmail.com>
2024-12-12 16:46:26 +0000

    Add exercise

---
+++ dictionaries/zad_dom.py
@@ -0,0 +1,67 @@
+# Chcemy pobrać pewne listę z API ale te API nie pozwala na zwracanie listy dłuższej niż 5 elementów
+# Zadanie polega na tym żeby zawsze móc pobrać całą listę zwirzątek
+
+from typing import Any
+
+# Symulacja bazy danych, która działa jako źródło danych dla naszego API
+database_api = {
+    1: ["lew", "tygrys", "słoń", "żółw", "koala"],
+    2: ["wilk", "lis", "zając", "jelen", "niedźwiedź"],
+    3: ["orzeł", "sokół", "kruk", "papuga", "pingwin"],
+    4: ["pies", "kot", "świnka morska", "chomik", "papuga"],
+    5: ["delfin", "rekin", "ośmiornica", "meduza", "krewetka"]
+}
+
+
+def get_request_api(batch_id: int) -> dict[str, Any]:
+    """
+    Pobiera dane z `database_api` na podstawie identyfikatora partii (batch_id).
+
+    Args:
+        batch_id (int): Identyfikator partii do pobrania.
+
+    Returns:
+        dict[str, Any]: Słownik z kluczami:
+            - `batch`: Lista zwierząt z danej partii lub `None`, jeśli partia nie istnieje.
+            - `next`: Identyfikator następnej partii lub `None`, jeśli to ostatnia partia.
+
+    Przykład:
+        >>> get_request_api(1)
+        {'batch': ["lew", "tygrys", "słoń", "żółw", "koala"], 'next': 2}
+        >>> get_request_api(5)
+        {'batch': ["delfin", "rekin", "ośmiornica", "meduza", "krewetka"], 'next': None}
+    """
+    batch = database_api.get(batch_id)
+    next_bach_id = batch_id + 1 if batch_id + 1 in database_api else None
+    return {
+        'batch': batch,
+        'next': next_bach_id
+    }
+
+
+def fetch_all_animals() -> list[str]:
+    """
+    Pobiera pełną listę zwierząt korzystając z ograniczonego API.
+
+    Funkcja iteracyjnie pobiera partie danych, aż do momentu, gdy żadna kolejna partia
+    nie będzie dostępna. Łączy wszystkie wyniki w jedną listę.
+
+    Returns:
+        list[str]: Pełna lista wszystkich zwierząt.
+
+    Przykład:
+        >>> fetch_all_animals()
+        ["lew", "tygrys", "słoń", "żółw", "koala", "wilk", "lis", ...]
+    """
+    all_animals = []
+
+    # Your code here!
+    # ...
+
+    return all_animals
+
+
+if __name__ == '__main__':
+    # Wywołanie funkcji i wyświetlenie pełnej listy zwierząt
+    animals = fetch_all_animals()
+    print("Pełna lista zwierząt:", animals)
