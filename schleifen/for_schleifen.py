d2ae4de453d673c49c18e610c32b7a951f0df7ee
Julia <juliagolemo61@gmail.com>
2024-12-15 17:24:59 +0000

    For schleifen part 1

---
+++ schleifen/for_schleifen.py
@@ -0,0 +1,13 @@
+# For Schleifen (benutzt um über Sequenzen (z.B. String, Liste, Tupel) mehrfach zu interieren)
+
+
+liste = ["Banane", "Weintrauben", "Nüsse"]
+
+# dla kazdego elementu po kolei w liscie zrob to i to.
+# Kiedy np. lista sie skonczy, nie musimy tak jak w while przerywac petli, tu sie sama zatrzyma
+for element in liste:
+    print(element)
+# wyswietlily nam sie wszytskie produkty
+
+
+
