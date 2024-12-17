12eaf9987bca73f485837ff7d2772b59d3fa2447
Julia <juliagolemo61@gmail.com>
2024-12-17 10:45:59 +0000

    Functions 1

---
+++ functions/functions.py
@@ -0,0 +1,23 @@
+# musimy najpierw zdefiniowac funkcje!! inaczej nam ja bedzie podkreslac
+
+def begruessung():
+    print("Hallo, nett dich kennenzulernen!")
+begruessung()
+
+# Tekst wyswietla nam sie w terminalu
+# Dlaczego print działa przez funkcję begruessung()?
+# kiedy pisze def, print, to dopiero TWORZE funkcje o nazwie begruessung.
+# Jednak samo zdefiniowanie funkcji niczego nie wykonuje.
+# dopiero kiedy napisze begruessung(), czyli WYWOLAM funckcje, wszystko sie wykonuje
+# podsumowanie
+# def definiuje funkcję, czyli przygotowuje pewne instrukcje do późniejszego wykonania.
+# Wywołanie funkcji (np. begruessung()) sprawia, że instrukcje w funkcji są wykonywane.
+# W środku funkcji może znajdować się print, ale to begruessung() wywołuje ten print.
+
+begruessung()
+begruessung()
+begruessung()
+
+# To po co pisac funkcje, skoro mozna print i na to samo wyjdzie?
+# Po to zeby szybciej wyswietlac zawartosc - czyli nie musimy calego dlugiego print za kazdym razem pisac
+# Wystraczy begruessung() i mamy
