92e6e46811fc8290c214300f903a57f430c17398
Julia <juliagolemo61@gmail.com>
2024-12-17 11:34:16 +0000

    Add docstrings

--- functions/taschenrechner.py
+++ functions/taschenrechner.py
@@ -13,7 +13,7 @@ print(summe)
 def hallo(name, alter):
     print(name)
     print(alter)
-hallo(name:"Julia", alter:23)
+hallo(name "Julia", alter:23)
 # Wyswietla nam sie po kolei imie, potem wiek
 
 # Tez nam sie po kolei wyswietla
@@ -22,3 +22,26 @@ def hallo(name = "Chiara", alter = 85):
     print(alter)
 hallo()
 
+# Moge tez tak:
+def hallo(name = "Chiara", alter = 85):
+    print(name)
+    print(alter)
+hallo(name = "Chiara", alter=85 )
+
+# Co jesli nie wiemy ile np liczb chcemy dodac? Wtedy po prostu dodaje zahlen ale z GWIAZDKA!!jesli nie wiem, ile czegos bedzie/jest, dodaje gwiazdke
+# Jak nie znam liczby argumentow -> gwiazdka
+
+def addieren(*zahlen):
+    print(sum(zahlen))
+addieren(20, 80, 6)
+# czemu w tutorialu byl : a tu nie moge miec, bo mi inaczej nie wychodzi?? okolo 3h 20min
+
+# ** daje wtedy, jak cos juz bylo wczesniej zdefiniowane
+def addieren(**daten):
+    print(daten)
+addieren(name = "Julia", alter = 23, hobby = "turnen")
+
+# DOCSTRINGS - längere Kommentare
+"""
+ofjoerf
+""""
