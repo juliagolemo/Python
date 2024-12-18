1f335e53c6710cddb6990cd7086de9324daf7fd6
Julia <juliagolemo61@gmail.com>
2024-12-18 16:14:46 +0000

    Spiel part 1

---
+++ spiel/spiel.py
@@ -0,0 +1,21 @@
+# Kolko i krzyzyk
+# 1. Spielbrett erstellen
+def erstelle_brett():
+    brett = []
+    for i in range(3):
+        zeile = [" ", " ", " "]
+        brett.append(zeile)
+    print(brett)
+erstelle_brett()
+
+
+
+
+
+
+
+
+# range() beschreibt eine Zaehlsequenz bis zum angegebenem Argument
+
+
+
