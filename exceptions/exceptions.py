642587052403374b2795934d3cd566126fb67cd4
Julia <juliagolemo61@gmail.com>
2024-12-19 13:30:06 +0000

    Exceptions 1

--- exceptions/exceptions.py
+++ exceptions/exceptions.py
@@ -1,1 +1,26 @@
-ergebnis = 10 / 0
+try:
+    ergebnis = 10 / 0
+except ZeroDivisionError:
+    print("Du darfst nicht durch 0 dividieren!")
+
+try:
+    ergebnis = 10 / 2
+except ZeroDivisionError:
+    print("Du darfst nicht durch 0 dividieren!")
+except TypeError:
+    print("Du kannst nur mit zahlen rechnen!")
+except:
+    print("Hier ist etwas schief gegangen.")
+else:
+    print("Kein Fehler, super!")
+
+try:
+    ergebnis = 10 / 2
+except ZeroDivisionError:
+    print("Du darfst nicht durch 0 dividieren!")
+except TypeError:
+    print("Du kannst nur mit zahlen rechnen!")
+except:
+    print("Hier ist etwas schief gegangen.")
+finally:
+    print("Alles ist durchgelaufen!")
