c716754be2d66cd7dc1483ab5d990a08a2ca3b84
Julia <juliagolemo61@gmail.com>
2024-12-19 16:30:14 +0000

    Add descriptions 1

---
+++ module/testfile.py
@@ -0,0 +1,8 @@
+import unser_modul
+import random
+from math import sqrt
+
+unser_modul.begruessung("Julia")
+result = sqrt(16)
+print(result)
+
---
+++ module/unser_modul.py
@@ -0,0 +1,6 @@
+def begruessung(name):
+    print(f"Hallo meine Liebe {name}")
+
+# To jest funkcja o nazwie begruessung, która przyjmuje jeden argument — name (czyli imię, które podasz).
+# Funkcja ta po prostu wypisuje powitanie w formie "Hallo meine Liebe" (co po niemiecku oznacza "Cześć moja droga") oraz dodaje imię, które zostało przekazane do funkcji.
+
