f943c93380ae0b2f5a3b329e79e97c8016dce3ff
Julia <juliagolemo61@gmail.com>
2024-12-23 07:50:04 +0000

    sort min zad

---
+++ sort/find_min_max.py
@@ -0,0 +1,9 @@
+def find_min(lista_intow: list[int]) -> int:
+    # float('inf') to nieskończoność na + (dodatnia)
+    min_int = float('inf')
+
+    return min_int
+
+
+if __name__ == '__main__':
+    print(find_min(lista_intow=[3, 4, 10, 32, 46, 2, 51, 4, 6]))
