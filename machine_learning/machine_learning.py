6643ca46b838916d92c871431aad10706ab07460
Julia <juliagolemo61@gmail.com>
2024-12-29 20:18:45 +0000

    Add confusion matrix

--- machine_learning/machine_learning.py
+++ machine_learning/machine_learning.py
@@ -5,8 +5,10 @@
 # Pip to narzędzie w Pythonie służące do zarządzania pakietami, umożliwia instalowanie, aktualizowanie i usuwanie bibliotek oraz innych modułów Pythona
 
 # Wirtualne srodowiska
-#Wirtualne środowiska w Pythonie to izolowane środowiska, które pozwalają na oddzielenie zależności i pakietów instalowanych dla różnych projektów. Dzięki temu każdy projekt może mieć własny zestaw bibliotek i wersji Pythona, niezależny od innych projektów i systemowych instalacji.
+# Wirtualne środowiska w Pythonie to izolowane środowiska, które pozwalają na oddzielenie zależności i pakietów instalowanych dla różnych projektów.
+# Dzięki temu każdy projekt może mieć własny zestaw bibliotek i wersji Pythona, niezależny od innych projektów i systemowych instalacji.
 
 # Jupyter Notebook
 # Jupyter Notebook to otwartoźródłowe narzędzie służące do interaktywnej pracy z kodem, tekstem, danymi i wizualizacjami
-# Jupyter Notebook jest narzędziem, które pomaga zrozumieć i pracować z danymi, kodem i wynikami w prosty, interaktywny sposób. Notebook może być przydatny, ponieważ łączy kod, opisy i wyniki w jednym miejscu, co czyni go świetnym narzędziem do nauki, analizy i prezentacji.
+# Jupyter Notebook jest narzędziem, które pomaga zrozumieć i pracować z danymi, kodem i wynikami w prosty, interaktywny sposób.
+# Notebook może być przydatny, ponieważ łączy kod, opisy i wyniki w jednym miejscu, co czyni go świetnym narzędziem do nauki, analizy i prezentacji.
