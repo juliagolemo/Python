6e06bf59dc0765c3f49b6893a4212d677424073f
Julia <juliagolemo61@gmail.com>
2025-02-07 19:43:10 +0000

    Add Czika and Pabi

--- zoo/zoo.py
+++ zoo/zoo.py
@@ -1,6 +1,8 @@
 from abc import ABC, abstractmethod
 
 # Klasa abstrakcyjna Zwierze
+
+
 class Zwierze(ABC):
     def __init__(self, imie: str, wiek: int, waga: float, wzrost: float):
         self.imie = imie
@@ -16,13 +18,17 @@ class Zwierze(ABC):
         pass
 
 # Klasa Zwierze_Ladowe dziedzicząca po Zwierze
+
+
 class Zwierze_Ladowe(Zwierze):
     def biegnij(self):
         szybkosc_biegu = (self.waga / 10) + (self.wzrost / 5)
         return f"{self.imie} biegnie z prędkością {szybkosc_biegu:.2f} km/h"
 
-# Klasa Zwierze_Latajace dziedzicząca po Zwierze_Ladowe
-class Zwierze_Latajace(Zwierze_Ladowe):
+# Klasa Zwierze_Latajace dziedzicząca po Zwierze
+
+
+class Zwierze_Latajace(Zwierze):
     def __init__(self, imie: str, wiek: int, waga: float, wzrost: float, rozpietosc_skrzydel: float):
         super().__init__(imie, wiek, waga, wzrost)
         self.rozpietosc_skrzydel = rozpietosc_skrzydel
@@ -30,4 +36,30 @@ class Zwierze_Latajace(Zwierze_Ladowe):
     def lec(self):
         szyb_lotu = (self.waga / 5) + (self.wzrost / 2)
         return f"{self.imie} leci z prędkością {szyb_lotu:.2f} km/h"
-    .
+
+
+class Pies(Zwierze_Ladowe):
+    def daj_glos(self):
+        return "Hau hau"
+
+pies = Pies("Czika", 9, 10, 40)
+print(pies.daj_glos())
+print(pies)
+
+class Krolik(Zwierze_Ladowe):
+    def daj_glos(self):
+        return "Piiii"
+
+
+krolik = Krolik("Pabi", 7, 2, 20)
+print(krolik.daj_glos())
+print(krolik)
+
+print(pies.biegnij())
+print(krolik.biegnij())
+
+class ZOO:
+    def __init__(self):
+        self.zwierzeta = []
+
+
